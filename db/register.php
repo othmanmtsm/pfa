c2bb6ed6cf868da63eca7210e53ef89a0b1d0458
othmanmtsm <mmotassimothman@gmail.com>
2020-03-22 00:31:35 +0100

    minor changes

--- db/register.php
+++ db/register.php
@@ -14,13 +14,12 @@
 <div class="container">
     <div class="row">
         <div style="height: 500px !important;" class="col-6 mx-auto text-center" id="loginf">
-        <form class="form-signin" method="POST">
+        <form class="form-signin pt-5" method="POST">
             <h3 class="mt-5">Register</h3>
             <hr/>
             <input type="text" name="user" class="form-control" placeholder="Username" required autofocus>
-            <input type="email" name="mail" class="form-control" placeholder="Username" required autofocus>
+            <input type="email" name="mail" class="form-control" placeholder="Mail" required autofocus>
             <input type="password" name="pass" class="form-control" placeholder="Password" required>
-            <input type="password" name="passC" class="form-control" placeholder="Confirm password" required>
             <button class="btn btn-lg btn-primary btn-block" type="submit">Sign up</button>
         </form>
         </div>
